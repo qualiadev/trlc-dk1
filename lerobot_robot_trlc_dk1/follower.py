mQualia/q-trlc-dk1

feat(plugin,deps): pin lerobot to mQualia fork + carry working dk1 patches
* pyproject.toml: pin 'lerobot' to mQualia/lerobot.git@5e781fc9 (the
  dk1-patches branch tip), so 'uv sync' is deterministic and our local
  patches against installed lerobot can no longer be clobbered by
  upstream main moving forward.

* lerobot_robot_trlc_dk1/leader.py: correct the leader motor model map.
  After the 2026-04-28 leader rebuild we probed both leader buses
  (/dev/ttyACM0 and /dev/ttyACM2) at 1 Mbaud and read the exact model
  number per id; the result is:
    id 1, 2, 5, 6 -> 1200 (xl330-m288)
    id 3, 4, 7    -> 1190 (xl330-m077)
  i.e. m077 / m288 are flipped compared to the previous config.

* lerobot_robot_trlc_dk1/follower.py: read all motor states with one
  pipelined batched refresh instead of looping per-motor refresh, which
  is ~7x faster on USB-CDC <-> CAN bridges where each non-blocking
  recv() previously round-tripped serially.

* lerobot_robot_trlc_dk1/bi_follower.py: read both arms (and send
  actions to both) in parallel threads, since left/right live on
  separate serial ports. Eliminates the serial-loop floor that capped
  the bi-arm + 3-camera record loop at ~13 Hz.

* lerobot_robot_trlc_dk1/motors/DM_Control_Python/DM_CAN.py: add
  MotorControl.pipelined_refresh(motors), the helper that
  follower.py/bi_follower.py rely on. Sends one refresh request per
  motor back to back, then drains the serial buffer until every
  motor's reply has been seen or the timeout elapses.

Tested on Thor (Jetson, JetPack 7) against the corcastaQ pi05
'put cork in box' policy: sustained ~9 fps inference loop with the
new pipelined+parallel reads vs the previous ~3-7 fps regression.

lerobot_robot_trlc_dk1/follower.py
@@ -184,9 +184,18 @@ class DK1Follower(Robot):
         # Read arm position
         start = time.perf_counter()
 
+        # Pipelined batched read: send all motor refresh requests back-to-back,
+        # then drain replies. This is ~7x faster than the previous loop on
+        # USB-CDC <-> CAN bridges where each non-blocking recv() round-tripped
+        # serially. See lerobot_robot_trlc_dk1/motors/DM_Control_Python/DM_CAN.py.
+        n_seen = self.control.pipelined_refresh(self.motors.values())
+        if n_seen != len(self.motors):
+            logger.warning(
+                f"{self} pipelined_refresh: only {n_seen}/{len(self.motors)} motors replied within timeout."
+            )
+
         obs_dict = {}
         for key, motor in self.motors.items():
-            self.control.refresh_motor_status(motor)
             if key == "gripper":
                 # Normalize gripper position between 1 (closed) and 0 (open)
                 obs_dict[f"{key}.pos"] = map_range(

qualiadev/trlc-dk1

lerobot_robot_trlc_dk1/follower.py
@@ -242,9 +242,18 @@ class DK1Follower(Robot):
         return obs
 
     def _get_observation_pos_vel(self) -> dict[str, Any]:
+        # Pipelined batched read: send all motor refresh requests back-to-back,
+        # then drain replies. This is ~7x faster than looping refresh_motor_status
+        # per motor on USB-CDC <-> CAN bridges where each non-blocking recv()
+        # round-trips serially. See motors/DM_Control_Python/DM_CAN.py.
+        n_seen = self._control.pipelined_refresh(self._motors.values())
+        if n_seen != len(self._motors):
+            logger.warning(
+                f"{self} pipelined_refresh: only {n_seen}/{len(self._motors)} motors replied within timeout."
+            )
+
         obs: dict[str, Any] = {}
         for key, motor in self._motors.items():
-            self._control.refresh_motor_status(motor)
             if key == "gripper":
                 obs[f"{key}.pos"] = _map_range(
                     motor.getPosition(),